ec5eb20eedbd0fa52cbb55b33d08eae18fc1c1b2
4BNaru <98478646+4BNaru@users.noreply.github.com>
2022-02-06 15:49:45 +0900

    First
    asdf

---
+++ Assets/Scenes/SilencePlaza_UdonProgramSources/Monochrome.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Monochrome : UdonSharpBehaviour
+{
+    public GameObject MC; 
+    void Start()
+    {
+        
+    }
+    public void MonochromeIO()
+    {
+        if (MC.activeInHierarchy)
+        {
+            MC.SetActive(false);
+        }
+        else
+        {
+            MC.SetActive(true);
+        }
+    }
+}
+
